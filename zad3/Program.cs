60958b511c939742a43bac7f20dd636bbc874323
Borisovna <hipsavita@gmail.com>
2023-02-13 21:56:27 +0300

    zad4 added

---
+++ zad3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите число");
+string inputNum;
+inputNum=Console.ReadLine();
+
+int num= Convert.ToInt32(inputNum);
+
+if (num %2==0)
+{
+   System.Console.WriteLine($"{num} четное число"); 
+}
+else 
+{
+    System.Console.WriteLine($"{num} нечетное число");
+}
---
+++ zad4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число");
+string inputNum;
+inputNum=Console.ReadLine();
+
+int num= Convert.ToInt32(inputNum);
+int startnum=1;
+
+while (startnum<=num)
+{
+ if(startnum%2==0)
+ System.Console.WriteLine (startnum+ " ");
+ 
+  startnum++;
+}
+
