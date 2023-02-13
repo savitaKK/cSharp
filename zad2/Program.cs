f2b6b042f3718f800090f60dd4060500dc174929
Borisovna <hipsavita@gmail.com>
2023-02-13 19:31:07 +0300

    oshibki ispravltny

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -1,7 +1,7 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-//a = 5; b = 7 -> max = 7
-//a = 2 b = 10 -> max = 10
-//a = -9 b = -3 -> max = -3
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 System.Console.WriteLine("Введите первое число");
 string inputValue1;
@@ -11,10 +11,15 @@ System.Console.WriteLine("Введите второе число");
 string inputValue2;
 inputValue2=Console.ReadLine();
 
+System.Console.WriteLine("Введите третье число");
+string inputValue2;
+inputValue2=Console.ReadLine();
+
 int value1= Convert.ToInt32(inputValue1);
 int value2= Convert.ToInt32(inputValue2);
+int value3= Convert.ToInt32(inputValue3);
 
-if (value1==value2)
+if (value1>=value2)
 {
    System.Console.WriteLine("Числа равны"); 
 }
