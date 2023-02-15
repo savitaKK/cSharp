3bcf22bb03272cfeb30eaf0ed5605b7a14260239
Borisovna <hipsavita@gmail.com>
2023-02-15 18:51:47 +0300

    added homework seminar 2, task 1

---
+++ zad2.1/hw2.1.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Validate(int num)
+{
+    if (num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это число не трехзначное");
+    return false;
+}
+
+int number = Prompt("Введите 3-хзначное число");
+if (Validate(number))
+{
+    int secondDigit = number /10 % 10;
+    System.Console.WriteLine($"средняя цифра числа {number} равна {secondDigit}");
+}
